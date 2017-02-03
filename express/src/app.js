722d17f7ae09751eb56d777022e96796bcc04f84
mty <mty@mty-MacBook-Pro.local>
2017-02-03 11:37:23 -0800

    Add cluster

--- express/src/app.js
+++ express/src/app.js
@@ -1,15 +1,23 @@
 'use strict';
 
+const cluster = require('cluster');
 const express = require('express');
 
-const apiRouter = require('./router/api.router');
-const webPageRouter = require('./router/web-page.router');
+if (cluster.isMaster) {
+    let cpuCounter = require('os').cpus().length;
+    for (let i = 0; i < cpuCounter; i++) {
+        cluster.fork();
+    }
+} else {
+    const apiRouter = require('./router/api.router');
+    const webPageRouter = require('./router/web-page.router');
 
-let app = express();
+    let app = express();
 
-app.use('/api', apiRouter);
-app.use('/', webPageRouter);
+    app.use('/api', apiRouter);
+    app.use('/', webPageRouter);
 
-app.listen(3000, function() {
-    console.log('Backend started');
-})
+    app.listen(3000, function() {
+        console.log('Server started at port: 3000');
+    });
+}
